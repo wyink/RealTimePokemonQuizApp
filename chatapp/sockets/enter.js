874a230764fb2fa0eab85e5f4e3b93b51bd81656
KenyaSugimoto <sk_5_24@yahoo.co.jp>
2020-11-05 21:11:25 +0900

    update login function

--- chatapp/sockets/enter.js
+++ chatapp/sockets/enter.js
@@ -2,7 +2,9 @@
 
 module.exports = function (socket) {
     // 入室メッセージをクライアントに送信する
-    socket.on('', function (data) {
+    socket.on('enterUserEvent', function (data) {
 
+        // 入室しましたメッセージ
+        socket.emit('receiveMessageEvent', data);
     });
 };
