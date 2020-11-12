4587cc2efa5da159763499be730760dc60049e26
Yamazaki-Koji0619 <kouzi619soccer@icloud.com>
2020-11-12 19:36:03 +0900

    fetch api

--- chatapp/public/javascripts/startApi.js
+++ chatapp/public/javascripts/startApi.js
@@ -2,9 +2,13 @@
 'use strict';
 
 const data = [];
+const PokeId = 1;
 
 function fetchPokeInfo() {
-    fetch('https://pokeapi.co/api/v2/pokemon-species/1').then(res => res.json())
+    //乱数作成関数　1〜893
+
+    //API情報取得
+    fetch(`https://pokeapi.co/api/v2/pokemon-species/${PokeId}`).then(res => res.json())
         .then(result => {
             console.log(result);
         })
