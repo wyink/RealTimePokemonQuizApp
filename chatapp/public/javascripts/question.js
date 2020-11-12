fd6ebeea13dcc75985e4426de531f0e9aa0161ac
KenyaSugimoto <45161125+KenyaSugimoto@users.noreply.github.com>
2020-11-12 22:16:12 +0900

    Update question.js

--- chatapp/public/javascripts/question.js
+++ chatapp/public/javascripts/question.js
@@ -1,4 +1,4 @@
-let PokeImgData = "";
+let pokemonJsonUrl = "https://raw.githubusercontent.com/fanzeyi/pokemon.json/master/images/";
 
 socket.on('receiveApiData', function (data) {
     // pokeIdの取得
@@ -8,6 +8,8 @@ socket.on('receiveApiData', function (data) {
     //ゼロパディング処理
     let ret = ( '000' + pokeId ).slice( -3 );
     //imgタグを表示
-    $('.class').apend("<img class='poke_img' src='../../public/pokemon.json/" + String(ret) + ".png' alt='' >"); //画像のパスの指定は勝手にやっちゃいました。すいません。byすぎ
+    $('.class').apend("<img class='poke_img' src='" + pokemonJsonUrl + String(ret) + ".png' alt='' >"); //画像のパスの指定は勝手にやっちゃいました。すいません。byすぎ
+    // $('.class').apend("<img class='poke_img' src='../../public/pokemon.json/" + String(ret) + ".png' alt='' >"); //画像のパスの指定は勝手にやっちゃいました。すいません。byすぎ
+
 });
 
