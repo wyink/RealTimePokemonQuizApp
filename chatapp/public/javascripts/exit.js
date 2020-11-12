1eab1792a7e5c42c9c3e40fcc51be9a8ab8b7048
Yamazaki-Koji0619 <kouzi619soccer@icloud.com>
2020-11-12 18:17:15 +0900

    exit

--- chatapp/public/javascripts/exit.js
+++ chatapp/public/javascripts/exit.js
@@ -3,14 +3,14 @@
 // 退室メッセージをサーバに送信する
 function exit() {
     // ユーザ名取得
-    const userName = '';
+    const userName = $("#userName").val();
     // 退室メッセージイベントを送信する
-
+    socket.emit('sendExitMessage', userName);
     // 退室
     location.href = '/';
 }
 
 // サーバから受信した退室メッセージを画面上に表示する
-socket.on('', function (data) {
-    $('#thread').prepend('<p>' + '</p>');
+socket.on('receiveExitMessage', function (data) {
+    $('#thread').prepend('<p>' + data + 'さんが退出しました。' + '</p>');
 });
--- chatapp/sockets/exit.js
+++ chatapp/sockets/exit.js
@@ -2,7 +2,8 @@
 
 module.exports = function (socket) {
     // 退室メッセージをクライアントに送信する
-    socket.on('', function (data) {
-
+    socket.on('sendExitMessage', function (data) {
+        //退出しましたメッセージ
+        socket.broadcast.emit('receiveExitMessage', data);
     });
 };
