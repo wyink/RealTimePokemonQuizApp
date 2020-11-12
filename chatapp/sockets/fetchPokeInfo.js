cf0acae0d569756d58b78e4b8ccfd5f42e44f1c8
KenyaSugimoto <sk_5_24@yahoo.co.jp>
2020-11-12 20:56:20 +0900

    fix fetchPokeInfo.js

--- chatapp/sockets/fetchPokeInfo.js
+++ chatapp/sockets/fetchPokeInfo.js
@@ -5,7 +5,7 @@ const pokeId = Math.floor(Math.random() * 809);
 console.log(`pokeId: ${pokeId}`);
 
 const options = {
-    url: `https://pokeapi.co/api/v2/pokemon-species/1`,
+    url: `https://pokeapi.co/api/v2/pokemon-species/${pokeId}`,
     method: "GET",
     headers: {
         "content-type": "application/json"
@@ -14,6 +14,7 @@ const options = {
 
 webclient(options, (error, response, body)=>{
     const pokeInfo = JSON.parse(response.body);
+    console.log(pokeInfo.names[0].name);
     console.log(pokeInfo);
     return pokeInfo;
 })
