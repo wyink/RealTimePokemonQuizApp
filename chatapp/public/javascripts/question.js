c49ea0f26baaf3b7a09bc7e0f5abcb480819ac02
KenyaSugimoto <sk_5_24@yahoo.co.jp>
2020-11-13 19:41:58 +0900

    update question.js

--- chatapp/public/javascripts/question.js
+++ chatapp/public/javascripts/question.js
@@ -7,6 +7,7 @@ function question() {
     console.log("start question.js");
     // 投稿内容を送信
     socket.emit('fetchPokeApiRequestEvent');
+
     socket.on('receivePokeApiData', function (data) {
         console.log(data);
 
@@ -21,10 +22,33 @@ function question() {
 
         const pokeImgUrl = pokemonJsonUrl + String(ret) + ".png";
         console.log(pokeImgUrl);
-        //imgタグを表示
 
-        $('#thread').prepend("<img class='poke-img' src='" + pokeImgUrl + "' alt='' >");
+        const questionArea = "<div class='question'><img class='poke-img' src='" + pokeImgUrl + "' alt='' ></div>"
+        //問題の表示
+        $('#thread').prepend(questionArea);
     });
 
     return false;
 }
+
+// socket.on('receivePokeApiData', function (data) {
+//     console.log(data);
+
+//     //APIからの情報取得
+//     // $('#thread').prepend(PokeId = data.id);
+//     const pokeId = data.id;
+//     console.log("pokeId", pokeId);
+
+//     //ゼロパディング処理
+//     let ret = ( '000' + pokeId ).slice( -3 );
+//     console.log("ret:", ret);
+
+//     const pokeImgUrl = pokemonJsonUrl + String(ret) + ".png";
+//     console.log(pokeImgUrl);
+
+//     const questionArea = "<div><img class='poke-img' src='" + pokeImgUrl + "' alt='' ></div>"
+//     //問題の表示
+//     $('#thread').prepend(questionArea);
+//     console.log("add img");
+// });
+
