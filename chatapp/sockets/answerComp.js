7e0edde989c668c62a4f045af3e2a5e33bb41a64
yuta <country7777iy@gmail.com>
2020-11-17 14:41:53 +0900

    pokedex.jsonの内容をリクエストで取得

--- chatapp/sockets/answerComp.js
+++ chatapp/sockets/answerComp.js
@@ -1,5 +1,4 @@
-const fs = require('fs');
-const path = require('path');
+const webclient = require('request');
 
 'use strict';
 
@@ -8,21 +7,28 @@ module.exports = function(socket, io){
 
     socket.on('sendAnswer',function(data){
         //解答ファイルの読み込み（pokemon.json)
-        const pokeJsonFile = path.join(__dirname,'..','public/pokemon.json/pokedex.json');
-        const pokeJson = JSON.parse(fs.readFileSync(pokeJsonFile,'utf8'));
-        
-        let judge = false; //正解していた場合はtrue,不正解はfalse
-        for(let poke of pokeJson){
-            if(poke.id == data.ANSID){
-                if(data.ANSWER==poke.name.japanese){
-                    judge = true;
-                }else{
-                    judge = false;
-                } 
-                break;
+        const options = {
+            url: 'https://raw.githubusercontent.com/fanzeyi/pokemon.json/master/pokedex.json',
+            method: "GET",
+            headers: {
+                "content-type": "application/json"
             }
-        }
-        io.sockets.emit('receiveAnswer',judge);
+        };
+        webclient(options, (error, response, body)=>{
+            const pokeJson = JSON.parse(response.body);
+            // フロント側にデータを送信
+            let judge = false; //正解していた場合はtrue,不正解はfalse
+            for(let poke of pokeJson){
+                if(poke.id == data.ANSID){
+                    if(data.ANSWER==poke.name.japanese){
+                        judge = true;
+                    }else{
+                        judge = false;
+                    } 
+                    break;
+                }
+            }
+            io.sockets.emit('receiveAnswer',judge);    
+        });
     });
-
 };
