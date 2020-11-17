6d24ea4fdaa8f53a6b67ddf63db4b099bb69bf68
KenyaSugimoto <sk_5_24@yahoo.co.jp>
2020-11-17 10:24:46 +0900

    fix

--- chatapp/public/javascripts/question.js
+++ chatapp/public/javascripts/question.js
@@ -26,31 +26,8 @@ socket.on('receivePokeApiData', function (data) {
     console.log(pokeImgUrl);
 
     const questionArea = `<div class='question'><img class='poke-img' src='${pokeImgUrl}' alt='' ></div>` ;
-    
+
     //問題の表示
     $('#Question > .question').replaceWith(questionArea);　//問題表示エリアに表示
     $('#thread').prepend(questionArea);　     //スレッド部分に表示
 });
-
-
-// socket.on('receivePokeApiData', function (data) {
-//     console.log(data);
-
-//     //APIからの情報取得
-//     // $('#thread').prepend(PokeId = data.id);
-//     const pokeId = data.id;
-//     console.log("pokeId", pokeId);
-
-//     //ゼロパディング処理
-//     let ret = ( '000' + pokeId ).slice( -3 );
-//     console.log("ret:", ret);
-
-//     const pokeImgUrl = pokemonJsonUrl + String(ret) + ".png";
-//     console.log(pokeImgUrl);
-
-//     const questionArea = "<div><img class='poke-img' src='" + pokeImgUrl + "' alt='' ></div>"
-//     //問題の表示
-//     $('#thread').prepend(questionArea);
-//     console.log("add img");
-// });
-
