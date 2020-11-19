47f159dd4cfbe6a2ea573d28ac38c4a719cc2744
yuta <country7777iy@gmail.com>
2020-11-19 20:22:24 +0900

    ポケモンのデータ源を一つに選定

--- chatapp/sockets/answerComp.js
+++ chatapp/sockets/answerComp.js
@@ -6,29 +6,37 @@ const webclient = require('request');
 module.exports = function(socket, io){
 
     socket.on('sendAnswer',function(data){
-        //解答ファイルの読み込み（pokemon.json)
+        
+        const pokeId = data.ANSID ;
+
+        //出題ソースと同じAPIにアクセスして丸ごとデータを取得
         const options = {
-            url: 'https://raw.githubusercontent.com/fanzeyi/pokemon.json/master/pokedex.json',
+            url: `https://pokeapi.co/api/v2/pokemon-species/${pokeId}`,
             method: "GET",
             headers: {
                 "content-type": "application/json"
             }
         };
+
         webclient(options, (error, response, body)=>{
-            const pokeJson = JSON.parse(response.body);
-            // フロント側にデータを送信
+            const pokeInfo = JSON.parse(response.body);
+
+            //入力と解答が一致するかどうかの確認
+            console.log(pokeInfo.names[0].name);
+            const truePokeName = pokeInfo.names[0].name;
+
             let judge = false; //正解していた場合はtrue,不正解はfalse
-            for(let poke of pokeJson){
-                if(poke.id == data.ANSID){
-                    if(data.ANSWER==poke.name.japanese){
-                        judge = true;
-                    }else{
-                        judge = false;
-                    } 
-                    break;
-                }
+            if(truePokeName == data.ANSWER){
+                judge = true;
+            }else{
+                judge = false;
             }
-            io.sockets.emit('receiveAnswer',judge);    
+
+            // フロント側にデータを送信
+            io.sockets.emit('receiveAnswer',judge);
+
         });
-    });
+
+    })
+
 };
