acf0901c488a42499fb2cfc7af18124fae4453e6
KenyaSugimoto <sk_5_24@yahoo.co.jp>
2020-11-19 21:06:56 +0900

    add requrstNowQuestion

--- chatapp/public/javascripts/enter.js
+++ chatapp/public/javascripts/enter.js
@@ -6,8 +6,19 @@ const userName = $("#userName").val();
 
 // 入室メッセージイベントを送信する
 socket.emit('enterUserEvent', userName + 'さんが入室しました。');
+socket.emit("checkStartedQuiz");
 
 // サーバから受信した入室メッセージを画面上に表示する
 socket.on('receiveMessageEvent', function (data) {
     $('#thread').prepend('<p>' + data + '</p>');
+});
+
+socket.on("returnStartedFlag", function(data) {
+    if (data) {
+        console.log("既にクイズが始まっているので、要求します。");
+        socket.emit("requestNowQuestion");
+    }
+    else {
+        console.log("まだ始まってません。");
+    }
 });
--- chatapp/sockets/fetchPokeInfo.js
+++ chatapp/sockets/fetchPokeInfo.js
@@ -33,6 +33,41 @@ module.exports = function (socket, io) {
         });
     });
 
+    socket.on("checkStartedQuiz", function() {
+        let started = false;
+        if (startedQuestion) {
+            console.log("既にクイズは始まっています");
+            started = true;
+        }
+        else {
+            console.log("まだ始まってません。");
+            started = false;
+        }
+        socket.emit("returnStartedFlag", started);
+    });
+
+    socket.on("requestNowQuestion", function() {
+        const pokeId = nowPokeId;
+
+        const options = {
+            url: `https://pokeapi.co/api/v2/pokemon-species/${pokeId}`,
+            method: "GET",
+            headers: {
+                "content-type": "application/json"
+            }
+        };
+
+        webclient(options, (error, response, body)=>{
+            const pokeInfo = JSON.parse(response.body);
+            console.log(pokeInfo);
+            console.log(pokeInfo.names[0].name);
+            // フロント側にデータを送信
+            io.sockets.emit('receivePokeApiData', pokeInfo);
+            console.log("complete send pokeInfo");
+        });
+
+    });
+
     return false
 
 
