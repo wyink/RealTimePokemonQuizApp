9a26fc34d61f1a61fb4e53d8a5735c103b8459f7
KenyaSugimoto <sk_5_24@yahoo.co.jp>
2020-11-22 04:33:50 +0900

    #4 fix

--- chatapp/public/javascripts/enter.js
+++ chatapp/public/javascripts/enter.js
@@ -3,6 +3,7 @@
 // 入室メッセージをサーバに送信する
 // 入力されたユーザ名を取得する
 const userName = $("#userName").val();
+console.log("userName", userName);
 
 // 入室メッセージイベントを送信する
 socket.emit('enterUserEvent', userName + 'さんが入室しました。');
