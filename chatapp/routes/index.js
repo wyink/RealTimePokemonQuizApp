a370a26b501d66259f6cf686911fcaf49c5457c8
yuta <country7777iy@gmail.com>
2020-11-24 06:13:51 +0900

    同じユーザ名の使用を不可にする処理

--- chatapp/routes/index.js
+++ chatapp/routes/index.js
@@ -12,6 +12,15 @@ router.get('/', function(request, response, next) {
 router.post('/room', function(request, response, next) {
     const userName = request.body.userName;
     console.log('ユーザ名：' + userName);
+    //ユーザ名がすでに使用されているかどうかをチェック
+    for(let col of global.userState){
+        if(col.USERNAME == userName){
+            response.render('index',{
+                isUsed:true
+            });
+        }
+    }
+
     response.render('room', { userName: userName });
 });
 
